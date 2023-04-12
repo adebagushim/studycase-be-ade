8a2df34c2da1fbf005fd7232a9d8929e83c0a184
adebagus <adebagushim333@gmail.com>
2023-04-12 22:35:04 +0700

    relasi db

--- controllers/productController.js
+++ controllers/productController.js
@@ -4,6 +4,8 @@ const { DATA_NOT_FOUND_CODE, GENERAL_ERROR_CODE } = require('../constant/errorCo
 const { BAD_REQUEST, ERROR_SERVER } = require('../constant/errorHttp');
 const { DATA_NOT_FOUND_MESSAGE, GENERAL_ERROR_MESSAGE } = require('../constant/errorMessage');
 const { number, generalMessage } = require('../constant/app');
+const Category = require('../models/category')
+const Tag = require('../models/tag')
 
 
 const byId = async (req, res, next) => {
@@ -25,7 +27,9 @@ const all = async (req, res, next) => {
         let data = await Product
         .find()
         .skip(parseInt(skip))
-        .limit(parseInt(limit));
+        .limit(parseInt(limit))
+        .populate('category')
+        .populate('tags');
         if (data && data.length === number.ZERO) {
             const error = new HttpError(DATA_NOT_FOUND_MESSAGE, DATA_NOT_FOUND_CODE, BAD_REQUEST);
             return next(error);
@@ -39,9 +43,32 @@ const all = async (req, res, next) => {
 }
 
 const create = async (req, res, next) => {
-    const { name, description, price, image_url } = req.body;
     try {
-        const payloadProduct = new Product({ name, description, price, image_url })
+        let payload = req.body;
+
+        if(payload.category){
+            let category = 
+            await Category
+            .findOne({name: {$regex: payload.category, $options: 'i'}});
+            if(category){
+                payload = {...payload, category: category._id};
+            } else {
+                delete payload.category;
+            }
+        }
+
+        if(payload.tags && payload.tags.length > 0){
+            let tags = 
+            await Tag
+            .find({name: {$in: payload.tags}});
+            if(tags.length){
+                payload = {...payload, tags: tags.map(tag => tag._id)};
+            } else {
+                delete payload.tags;
+            }
+        }
+
+        const payloadProduct = new Product({...payload })
         const data = await payloadProduct.save();
         req.data = data;
         next();
@@ -53,9 +80,32 @@ const create = async (req, res, next) => {
 
 async function update(req, res, next) {
     const id = req.params.pid;
-    const { name, description, price, image_url } = req.body;
+    let payload = req.body;
+    
     try {
-        let data = await Product.findByIdAndUpdate(id, { name, description, price, image_url }, {
+        if(payload.category){
+            let category = 
+            await Category
+            .findOne({name: {$regex: payload.category, $options: 'i'}});
+            if(category){
+                payload = {...payload, category: category._id};
+            } else {
+                delete payload.category;
+            }
+        }
+
+        if(payload.tags && payload.tags.length > 0){
+            let tags = 
+            await Tag
+            .find({name: {$in: payload.tags}});
+            if(tags.length){
+                payload = {...payload, tags: tags.map(tag => tag._id)};
+            } else {
+                delete payload.tags;
+            }
+        }
+        
+        let data = await Product.findByIdAndUpdate(id, {...payload}, {
             new: true
         });
         req.data = data;
--- models/product.js
+++ models/product.js
@@ -2,21 +2,36 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const productSchema = new Schema({
+    
     name: {
         type: String,
         minlength: [3, 'Panjang nama product minimal 3 karakter'],
         maxlength: [20, 'Panjang nama product maximal 20 karakter'],
         required: [true, 'Nama product harus diisi']
     },
+
     description: {
         type: String,
         maxlenght: [1000, 'Panjang deskripsi maximal 1000 karakter']
     },
+
     price: {
         type: Number,
         default: 0
     },
-    image_url: String
-});
+
+    image_url: String,
+
+    category: {
+        type: Schema.Types.ObjectId,
+        ref: 'Category'
+    },
+
+    tags: {
+        type: Schema.Types.ObjectId,
+        ref: 'Tag'
+    }
+
+}, { timestamps: true }); 
 
 module.exports = mongoose.model('Product', productSchema);
